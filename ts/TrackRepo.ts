1b231d531b1ac0e4ce178ec713999e9bfe67911d
Jason Holloway <miceeatnicerice@hotmail.com>
2023-11-18 20:08:18 +0000

    newly reactive rendering; topbar still to do

--- ts/TrackRepo.ts
+++ ts/TrackRepo.ts
@@ -1,4 +1,4 @@
-import { from, map } from "rxjs";
+import { Observable, ReplaySubject, combineLatest, concatMap, from, interval, map, merge, scan, startWith } from "rxjs";
 import { JobQueue } from "./queue";
 import { Playable, Track } from "./record";
 import { Store } from "./store";
@@ -11,42 +11,81 @@ export default class TrackRepo {
   private bid: string|undefined = undefined;
   private store: Store;
   private jobs: JobQueue;
-  private tracks: Track[] = [];
+  private tracks = new ReplaySubject<Track>();
 
   constructor(store:Store, jobs: JobQueue) {
     this.store = store;
     this.jobs = jobs;
   }
 
-  async setBand(bid: string, sink:()=>void): Promise<void> {
-    if(bid == this.bid) return;
-
-    this.bid = bid;
-
-    const allLoaded = await this.store.loadTracks(bid);
-
-    this.tracks = allLoaded
-      .map(p => Track.init(
-        p.info,
-        this.store,
-        this.jobs,
-        x => [
-          new Playable(x, () => this.store.loadBlobs({ stream: p.info.tid, idx: 0 }).pipe(map(sb => sb.blob))),
-          p.persistState
-        ]
-      ));
+  getTracks(bid: string): Observable<Track[]> {
+    return combineLatest([
+      interval(10000).pipe(
+        startWith(0),
+        concatMap(() =>
+          from(this.store.loadTracks(bid)).pipe(
+            map(r => r.map(p => Track.init(
+              p.info,
+              this.store,
+              this.jobs,
+              x => [
+                new Playable(x, () => this.store.loadBlobs({ stream: p.info.tid, idx: 0 }).pipe(map(sb => sb.blob))),
+                p.persistState
+              ]
+            )))
+          )),
+        startWith([])
+      ),
+      this.tracks.pipe(
+        scan((ac, t) => [...ac, t], <Track[]>[]),
+        startWith([])
+      )
+    ]).pipe(
+      map(([l, r]) => [...l, ...r])
+    );
   }
 
-  unsetBand() {
-    this.bid = undefined;
-    this.tracks = [];
-  }
-
+  //todo the track should be aggregated into the expected band!
   add(track:Track) {
-    this.tracks.push(track);
+    console.info('ADD TRACK')
+    this.tracks.next(track);
   }
 
-  list() {
-    return this.tracks;
-  }
+
+  // async setBand(bid: string, sink:()=>void): Promise<void> {
+  //   if(bid == this.bid) return;
+
+  //   this.bid = bid;
+
+  //   const allLoaded = await this.store.loadTracks(bid);
+
+  //   this.tracks = allLoaded
+  //     .map(p => Track.init(
+  //       p.info,
+  //       this.store,
+  //       this.jobs,
+  //       x => [
+  //         new Playable(x, () => this.store.loadBlobs({ stream: p.info.tid, idx: 0 }).pipe(map(sb => sb.blob))),
+  //         p.persistState
+  //       ]
+  //     ));
+
+  //   // so with band set
+  //   // we should start loading from dynamo
+  //   // can imagine a stream of renderables being returned
+  //   // in true reactive style
+  //   //
+  //   // this would replace the sink func
+  //   // and would be joined into 
+  //   //
+  // }
+
+  // unsetBand() {
+  //   this.bid = undefined;
+  //   this.tracks = [];
+  // }
+
+  // list() {
+  //   return this.tracks;
+  // }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -5,6 +5,7 @@ import { Store, openStore } from "./store"
 import { Band, Session, User } from "./model";
 import { JobQueue, runJobQueue } from "./queue";
 import TrackRepo from "./TrackRepo"
+import { BehaviorSubject, EMPTY, Observable, concatMap, empty, expand, from, fromEventPattern, reduce, throwError, withLatestFrom } from "rxjs";
 
 let audio: AudioContext|undefined;
 
@@ -21,133 +22,176 @@ let tracks: TrackRepo;
 const serverUrl = "http://localhost:9999";
 const googleAuthClientId = '633074721949-f7btgv29kucgh6m10av4td9bi88n903d.apps.googleusercontent.com';
 
+
+type PageType = string;
+
+
+const page$ = new BehaviorSubject<PageType>('resume');
+
+
+
+//todo throttle to 1
+const topDiv$ = from([document.createElement('div') as HTMLElement]);
+
+const mainDiv$ = page$.pipe(
+  expand(m => 
+    typeof m === 'string' ? renderPage(m) : EMPTY
+  ),
+
+  concatMap(m =>
+    typeof m !== 'string' ? [m] : EMPTY
+    )
+);
+
+
 window.onload = async () => {
-  await render('resume');
-};
-    
-async function render(nextPage?: typeof page): Promise<void> {
-  if(nextPage) { page = nextPage }
-  
   const divTop = document.getElementById('topBar')!;
   const divMain = document.getElementById('main')!;
 
-  divTop.innerHTML = '';
-  divMain.innerHTML = '';
-  
+  await Promise.all([
+    topDiv$.forEach(el => {
+      divTop.innerHTML = '';
+      divTop.appendChild(el);
+    }),
+
+    mainDiv$.forEach(el => {
+      divMain.innerHTML = '';
+      divMain.appendChild(el);
+    })
+  ]);
+};
+
+
+
+function renderPage(page: PageType): Observable<HTMLElement|PageType> {
   switch(page) {
-    case 'login':
-      const googleToken = await getGoogleToken();
+    case 'login': return renderLogin();
+    case 'logout': return renderLogout();
+    case 'resume': return renderResume();
+    case 'user': return renderUser();
+    case 'band': return renderBand();
+    default: return throwError(() => Error(`Bad page ${page}`));
+  }
+}
+  
+function renderLogin(): Observable<PageType> {
+  return from((async _ => {
+    const googleToken = await getGoogleToken();
 
-      session = await createSession('google', googleToken);
-      saveSession(session);
+    session = await createSession('google', googleToken);
+    saveSession(session);
 
-      return await render('resume');
+    return 'resume';
+  })());
+}
 
-		case 'logout':
-			clearSession();
-			//todo close store and job queue here
-			return await render('login');
+function renderLogout(): Observable<PageType> {
+  return from((async _ => {
+    clearSession();
+    return 'login';
+  })());
+}
 
-    case 'resume':
-      const summoned = trySummonSession();
-      if(!summoned) {
-        return await render('login');
-      }
+function renderResume(): Observable<PageType> {
+  return from((async _ => {
+    const summoned = trySummonSession();
+    if(!summoned) {
+      return 'login';
+    }
 
-      session = summoned;
-			const uid = session.uid;
+    session = summoned;
+    const uid = session.uid;
 
-			store = await openStore(uid);
-			jobs = await runJobQueue(uid, job => {
-				console.log('Handling job', job);
+    store = await openStore(uid);
+    jobs = await runJobQueue(uid, job => {
+      console.log('Handling job', job);
 
-				//todo some kind of branching on job type
-				return Track.createJobHandler({ store, jobs })(job);
-			})
+      //todo some kind of branching on job type
+      return Track.createJobHandler({ store, jobs })(job);
+    })
 
-      tracks = new TrackRepo(store, jobs);
+    tracks = new TrackRepo(store, jobs);
 
-      const dynamo = new DynamoDBClient({
-        region: 'eu-west-1',
-        credentials: session.awsCreds
-      });
+    const dynamo = new DynamoDBClient({
+      region: 'eu-west-1',
+      credentials: session.awsCreds
+    });
 
-      const loaded = await loadUser(dynamo, uid);
-      if(!loaded) {
-        return await render('logout');
-      }
+    const loaded = await loadUser(dynamo, uid);
+    if(!loaded) {
+      return 'logout';
+    }
 
-      user = loaded;
-      return await render('user');
+    user = loaded;
+    return 'user';
+  })());
+}
 
-    case 'user':
-      renderTopBar();
+function renderUser(): Observable<PageType|HTMLElement> {
+    // renderTopBar();
       
-      const bandsDiv = document.createElement('div');
-      bandsDiv.childNodes.forEach(n => n.remove());
+  const bandsDiv = document.createElement('div');
+  const ul = document.createElement('ul');
 
-      const ul = document.createElement('ul');
+  for(let [bid, bn] of user.bands.entries()) {
+    const li = document.createElement('li');
 
-      for(let [bid, bn] of user.bands.entries()) {
-        const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = '#';
+    a.onclick = async () => {
+      band = {
+        bid,
+        name: bn
+      }
+      page$.next('band');
+    };
+    a.innerText = bn;
+    li.appendChild(a);
 
-        const a = document.createElement('a');
-        a.href = '#';
-        a.onclick = async () => {
-          band = {
-            bid,
-            name: bn
-          }
-          await render('band');
-        };
-        a.innerText = bn;
-        li.appendChild(a);
+    ul.appendChild(li);
+  };
 
-        ul.appendChild(li);
-      };
+  bandsDiv.appendChild(ul);
 
-      bandsDiv.appendChild(ul);
+  const bandNameInput = document.createElement('input');
+  bandNameInput.type = 'text';
+  bandNameInput.placeholder = 'Band name...';
 
-      const bandNameInput = document.createElement('input');
-      bandNameInput.type = 'text';
-      bandNameInput.placeholder = 'Band name...';
+  const createBand = document.createElement('input');
+  createBand.type = 'button';
+  createBand.value = 'Create Band!';
 
-      const createBand = document.createElement('input');
-      createBand.type = 'button';
-      createBand.value = 'Create Band!';
+  createBand.onclick = async () => {
+    const r = await fetch(`${serverUrl}/band`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: bandNameInput.value
+      }),
+      credentials: 'include',
+      mode: 'cors'
+    })
+  };
 
-      createBand.onclick = async () => {
+  bandsDiv.appendChild(bandNameInput);
+  bandsDiv.appendChild(createBand);
 
-        const r = await fetch(`${serverUrl}/band`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name: bandNameInput.value
-          }),
-          credentials: 'include',
-          mode: 'cors'
-        })
-      };
+  return from([bandsDiv]);
+}
 
-      bandsDiv.appendChild(bandNameInput);
-      bandsDiv.appendChild(createBand);
-
-      divMain.appendChild(bandsDiv);
-      break;
-
-    case 'band':
-      await tracks.setBand(band.bid, () => render());
-
-      renderTopBar();
+function renderBand(): Observable<HTMLElement> {
+  return tracks.getTracks(band.bid) //from(tracks.setBand(band.bid, () => {}))
+    .pipe(concatMap(trackList => {
+      // renderTopBar();
 
       const header = document.createElement('h1');
       header.innerText = band.name;
       
       const recordingsUl = document.createElement('ul');
 
-      for(const { info, state } of tracks.list()) {
+      for(const { info, state } of trackList) {
         const li = document.createElement('li');
         li.innerHTML = info.tid;
 
@@ -179,36 +223,215 @@ async function render(nextPage?: typeof page): Promise<void> {
 
       button.onclick = async () => {
         const track = await Track.record(band.bid, store, jobs);
-        track.onchange = () => render();
-        tracks.add(track);
+        // track.onchange = () => page$.next();
+        tracks.add(track); //this should cause further tracks to appear
 
-        await render();
+        // await render();
       };
 
-      divMain.appendChild(header);
-      divMain.appendChild(recordingsUl);
-      divMain.appendChild(button);
+      const div = document.createElement('div');
+      div.appendChild(header);
+      div.appendChild(recordingsUl);
+      div.appendChild(button);
+      return [div];
+    }));
 
-      break;
-  }
-
-  function renderTopBar() {
-    const nameSpan = document.createElement('span');
-    nameSpan.innerText = session.uid;
-
-    const logoutButton = document.createElement('input');
-    logoutButton.type = 'button';
-    logoutButton.value = 'Log out';
-    logoutButton.onclick = () => render('logout');
-
-    const timeSpan = document.createElement('span');
-    setInterval(() => timeSpan.innerHTML = Date.now().toString(), 1000);
-
-    divTop.appendChild(nameSpan);
-    divTop.appendChild(logoutButton);
-    divTop.appendChild(timeSpan);
-  }
 }
+
+
+
+
+    
+// async function render(nextPage?: typeof page): Promise<void> {
+//   if(nextPage) { page = nextPage }
+  
+//   const divTop = document.getElementById('topBar')!;
+//   const divMain = document.getElementById('main')!;
+
+//   divTop.innerHTML = '';
+//   divMain.innerHTML = '';
+  
+//   switch(page) {
+//     case 'login':
+//       const googleToken = await getGoogleToken();
+
+//       session = await createSession('google', googleToken);
+//       saveSession(session);
+
+//       return await render('resume');
+
+// 		case 'logout':
+// 			clearSession();
+// 			//todo close store and job queue here
+// 			return await render('login');
+
+//     case 'resume':
+//       const summoned = trySummonSession();
+//       if(!summoned) {
+//         return await render('login');
+//       }
+
+//       session = summoned;
+// 			const uid = session.uid;
+
+// 			store = await openStore(uid);
+// 			jobs = await runJobQueue(uid, job => {
+// 				console.log('Handling job', job);
+
+// 				//todo some kind of branching on job type
+// 				return Track.createJobHandler({ store, jobs })(job);
+// 			})
+
+//       tracks = new TrackRepo(store, jobs);
+
+//       const dynamo = new DynamoDBClient({
+//         region: 'eu-west-1',
+//         credentials: session.awsCreds
+//       });
+
+//       const loaded = await loadUser(dynamo, uid);
+//       if(!loaded) {
+//         return await render('logout');
+//       }
+
+//       user = loaded;
+//       return await render('user');
+
+//     case 'user':
+//       renderTopBar();
+      
+//       const bandsDiv = document.createElement('div');
+//       bandsDiv.childNodes.forEach(n => n.remove());
+
+//       const ul = document.createElement('ul');
+
+//       for(let [bid, bn] of user.bands.entries()) {
+//         const li = document.createElement('li');
+
+//         const a = document.createElement('a');
+//         a.href = '#';
+//         a.onclick = async () => {
+//           band = {
+//             bid,
+//             name: bn
+//           }
+//           await render('band');
+//         };
+//         a.innerText = bn;
+//         li.appendChild(a);
+
+//         ul.appendChild(li);
+//       };
+
+//       bandsDiv.appendChild(ul);
+
+//       const bandNameInput = document.createElement('input');
+//       bandNameInput.type = 'text';
+//       bandNameInput.placeholder = 'Band name...';
+
+//       const createBand = document.createElement('input');
+//       createBand.type = 'button';
+//       createBand.value = 'Create Band!';
+
+//       createBand.onclick = async () => {
+
+//         const r = await fetch(`${serverUrl}/band`, {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             name: bandNameInput.value
+//           }),
+//           credentials: 'include',
+//           mode: 'cors'
+//         })
+//       };
+
+//       bandsDiv.appendChild(bandNameInput);
+//       bandsDiv.appendChild(createBand);
+
+//       divMain.appendChild(bandsDiv);
+//       break;
+
+//     case 'band':
+
+//       // tracks.getTracks(band.bid).forEach(r => render());
+
+//       // overall system should have one subscription at bottom
+//       // receiving pages and their versions
+      
+//       await tracks.setBand(band.bid, () => render());
+
+//       renderTopBar();
+
+//       const header = document.createElement('h1');
+//       header.innerText = band.name;
+      
+//       const recordingsUl = document.createElement('ul');
+
+//       for(const { info, state } of tracks.list()) {
+//         const li = document.createElement('li');
+//         li.innerHTML = info.tid;
+
+//         if(isPlayable(state)) {
+//           const b = document.createElement('input');
+//           b.type = 'button';
+//           b.value = 'Play';
+//           b.onclick = async () => {
+//             await state.play(audio ??= new AudioContext());
+//           };
+
+//           li.appendChild(b);
+//         }
+//         else {
+//           const b = document.createElement('input');
+//           b.type = 'button';
+//           b.value = 'Stop';
+//           b.onclick = () => state.stop();
+
+//           li.appendChild(b);
+//         }
+
+//         recordingsUl.appendChild(li);
+//       }
+
+//       const button = document.createElement('input');
+//       button.type = 'button';
+//       button.value = 'Record';
+
+//       button.onclick = async () => {
+//         const track = await Track.record(band.bid, store, jobs);
+//         track.onchange = () => render();
+//         tracks.add(track);
+
+//         await render();
+//       };
+
+//       divMain.appendChild(header);
+//       divMain.appendChild(recordingsUl);
+//       divMain.appendChild(button);
+
+//       break;
+//   }
+
+//   function renderTopBar() {
+//     const nameSpan = document.createElement('span');
+//     nameSpan.innerText = session.uid;
+
+//     const logoutButton = document.createElement('input');
+//     logoutButton.type = 'button';
+//     logoutButton.value = 'Log out';
+//     logoutButton.onclick = () => render('logout');
+
+//     const timeSpan = document.createElement('span');
+//     setInterval(() => timeSpan.innerHTML = Date.now().toString(), 1000);
+
+//     divTop.appendChild(nameSpan);
+//     divTop.appendChild(logoutButton);
+//     divTop.appendChild(timeSpan);
+//   }
+// }
 
 function trySummonSession(): Session|false {
   const found = window.localStorage.getItem('upis_session');
